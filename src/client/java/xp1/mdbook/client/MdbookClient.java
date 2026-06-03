07201f9ac9130bf966b47652d939798ed5f8ed46
tinayeorg <38555284+tianyeorg@users.noreply.github.com>
2026-06-03 19:47:26 +0800

    Add files via upload

---
+++ src/client/java/xp1/mdbook/client/MdbookClient.java
@@ -0,0 +1,10 @@
+package xp1.mdbook.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MdbookClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
